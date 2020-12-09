3b493f823e197479d7ae31662628d35d3eaf166d
jory999 <cmdeny21@gmail.com>
2020-12-10 03:07:56 +0800

    Code Finish

--- compilepractice.cpp
+++ compilepractice.cpp
@@ -104,12 +104,122 @@ Token Token_stream::get(){
     }
 }
 
+Token& Token_stream::current()
+{
+
+    return ct;
+}
+
+
 ////////////////////////////
 
 Token_stream ts{cin};
 
+////////////////////////
+double expr(bool get);
+double prim(bool get){
+    if(get) ts.get();
+
+    switch(ts.current().kind){
+
+        case Kind::number:
+         {
+            double v=ts.current().number_value;
+            ts.get();
+            return v;
+
+         }
+        case Kind::name:
+         {
+
+             double& v=table[ts.current().string_value];
+             if(ts.get().kind==Kind::assign) v=expr(true);
+             return v;
+         } 
+        case Kind::minus:
+            return -prim(true);
+        case Kind::lp:
+         {
+
+           auto e=expr(true);
+           if(ts.current().kind!=Kind::rp) return error("')' expected");
+           ts.get();
+           return e;
+
+         }  
+
+         default:
+
+          return error("primary expected");   
+
+    }
+
+}
+
+double term(bool get){
+
+      double left=prim(get);
+
+      for(;;){
+
+          switch(ts.current().kind){
+              case Kind::mul:
+                left*=prim(true);
+                break;
+              case Kind::minus:
+                 if(auto d=prim(true)){
+                 left /=d; 
+                 break;
+                 } 
+                error("devide by 0"); 
+              default:
+                 return left;  
+
+
+          }
+      }
+
+}
+
+double expr(bool get){
+
+      double left=term(get);
+
+      for(;;){
+
+          switch(ts.current().kind){
+              case Kind::plus:
+
+                 left+=term(true);
+                 break;
+              case Kind::minus:
+
+                 left-=term(true);
+                 break;
+
+               default:
+                  return left;
+
+
+
+          }
+      }
+
+
+}
+
+///////////////////////////////////////
+
 void calculate(){
     
+    for(;;){
+        ts.get();
+        if(ts.current().kind==Kind::end) break;
+        if(ts.current().kind==Kind::print) continue;
+        cout<<expr(false)<<'\n';
+
+
+    }
 
 }
 
